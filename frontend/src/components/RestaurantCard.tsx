fadce6071c3442638743fa278ac9b70a0588a630
GHOST <3089308393@qq.com>
2025-05-23 08:17:51 +0800

    feat: 完成推荐卡片的制作，跳转页面待做

--- frontend/src/components/RestaurantCard.tsx
+++ frontend/src/components/RestaurantCard.tsx
@@ -1,7 +1,7 @@
 import { View } from "@tarojs/components";
 import './RestaurantCard.scss'
-import React, { useState, useEffect } from 'react';
-import { Button, Skeleton, Space } from 'antd';
+import { useState, useEffect } from 'react';
+import { Skeleton, Rate } from 'antd';
 
 export default function RestaurantCard({ cardData }) {
   const [loading, setLoading] = useState<boolean>(false);
@@ -21,16 +21,25 @@ export default function RestaurantCard({ cardData }) {
 
   return (
     <Skeleton loading={loading} active={true} className="restaurant-card-skeleton">
-      <View className="restaurant-card" style={{ backgroundImage: `url(${cardData.image})` }}>
+      <View className="restaurant-card-container">
+        <View className="restaurant-card" style={{ backgroundImage: `url(${cardData.image})` }}></View>
         <View className="content-container">
           <View className="restaurant-name">{cardData.name}</View>
+          <View className="restaurant-rating">
+            <Rate disabled allowHalf defaultValue={cardData.rating} />
+            <View className="rating-count">{cardData.rating}</View>
+          </View>
           <View className="other-info">
-            <View className="restaurant-rating">评分: {cardData.rating}</View>
             <View className="restaurant-price">人均: {cardData.pricePerPerson}元</View>
             <View className="restaurant-distance">距离: {cardData.distance}</View>
           </View>
           <View className="restaurant-reasons">
-            {cardData.reasons[0]}
+            <View className="restaurant-reason">
+              {cardData.reasons[0]}
+            </View>
+            <View className="restaurant-reason">
+              {cardData.reasons[1]}
+            </View>
           </View>
         </View>
       </View>
